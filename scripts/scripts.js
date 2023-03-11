229a305788e1ec1724a46b6427f16e1190097f25
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-03-11 15:50:42 -0800

    added more stuff

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// const sprite = document.getElementById("sprite");
+// const frames = [
+//   "gpt-idle-frame-1.png",
+//   "gpt-idle-frame-3.png",
+//   "gpt-idle-frame-4.png",
+//   "gpt-idle-frame-5.png",
+//   "gpt-idle-frame-6.png",
+//   "gpt-idle-frame-7.png",
+// ];
+// let index = 0;
+
+// function animateSprite() {
+//   sprite.src = frames - [index];
+//   index++;
+//   if (index == frames.length) {
+//     index = 0;
+//   }
+// }
+
+// setInterval(animateSprite, 1000);
