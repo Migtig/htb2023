be860da6abfded5734694fbe2eef12141ca11912
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-03-11 15:59:47 -0800

    animation for GPT

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,20 @@
 "use strict";
 
-// const sprite = document.getElementById("sprite");
-// const frames = [
-//   "gpt-idle-frame-1.png",
-//   "gpt-idle-frame-3.png",
-//   "gpt-idle-frame-4.png",
-//   "gpt-idle-frame-5.png",
-//   "gpt-idle-frame-6.png",
-//   "gpt-idle-frame-7.png",
-// ];
-// let index = 0;
+const sprite = document.getElementById("sprite");
+const frames = [
+  "gpt-idle-frame1.png",
+  "gpt-idle-frame2.png",
+  "gpt-idle-frame3.png",
+  "gpt-idle-frame4.png",
+];
+let index = 0;
 
-// function animateSprite() {
-//   sprite.src = frames - [index];
-//   index++;
-//   if (index == frames.length) {
-//     index = 0;
-//   }
-// }
+function animateSprite() {
+  sprite.src = "images/sprites/" + frames[index];
+  index++;
+  if (index == frames.length) {
+    index = 0;
+  }
+}
 
-// setInterval(animateSprite, 1000);
+setInterval(animateSprite, 1000);
