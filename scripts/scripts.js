8b82cd682b9af519822da3a0fda22c26f05366e2
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-03-11 20:03:30 -0800

    some stuff

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const gptSprite = document.getElementById("gpt-sprite");
+const startGameBtn = document.getElementById("start-game");
+const startMenu = document.getElementById("start-menu");
+const introScene = document.getElementById("intro-scene");
+const continueBtn = document.getElementById("continue");
+const gameBox = document.getElementById("game-box");
+const wrapper = document.querySelector(".wrapper");
+
 const frames = [
   "gpt-idle-frame1.png",
   "gpt-idle-frame2.png",
@@ -18,3 +25,14 @@ function animateSprite() {
 }
 
 setInterval(animateSprite, 400);
+
+startGameBtn.addEventListener("click", function () {
+  startMenu.style.display = "none";
+  introScene.style.display = "block";
+});
+
+continueBtn.addEventListener("click", function () {
+  introScene.style.display = "none";
+  gameBox.style.display = "block";
+  wrapper.style.maxWidth = "800px";
+});
